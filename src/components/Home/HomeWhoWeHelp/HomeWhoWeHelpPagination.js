c15e1ee29360a1f9b2aa8e3608ee89decb1d9d67
duszaemilia <dusza.emilia@gmail.com>
2023-07-18 11:59:13 +0200

    HomeWhoWeHelp styling  with pagination

--- src/components/Home/HomeWhoWeHelp/HomeWhoWeHelpPagination.js
+++ src/components/Home/HomeWhoWeHelp/HomeWhoWeHelpPagination.js
@@ -1,16 +1,22 @@
 import React, {useState} from "react";
+import OrganizationsVerse from "./OrganizationsVerse";
 
-export default function HomeWhoWeHelpPagination({institutions}) {
+export default function WhoWeHelpDisplay({ institutions }) {
 
-    const [rowsNumber] = useState(3);
+    const [rowsNumber] =useState(3);
     const [currentPage, setCurrentPage] = useState(1)
-    const [pageNumber] = useState(Math.ceil(institutions.length / rowsNumber))
+    const [pageNumber] = useState(Math.ceil(institutions.length/rowsNumber))
+    const [pageSet, setPageSet] = useState([institutions[0], institutions[1], institutions[2]])
 
-    const handleChangePage = (event) => {
-        const clickedPage = Number(event.target.innerText);
-        setCurrentPage(Number(event.target.innerText));
+    const handleChangePage = (e) => {
+        const clickedPage = Number(e.target.innerText);
+        const lastIndex = clickedPage*rowsNumber;
+        const firstIndex = lastIndex - rowsNumber;
+        const newSet = institutions.slice(firstIndex, lastIndex)
 
-         }
+        setCurrentPage(Number(e.target.innerText))
+        setPageSet([...newSet]);
+    }
 
     const pages = () => {
         if (pageNumber <= 1) {
@@ -18,42 +24,15 @@ export default function HomeWhoWeHelpPagination({institutions}) {
         } else {
             let numbers = []
             for (let i = 1; i <= pageNumber; i++) {
-                numbers.push(
-                    <button
-                        className={`btn ${currentPage === i ? "btn-active" : ""}`}
-                        key={i}
-                        onClick={handleChangePage}>{i}
-                    </button>)
+                numbers.push(<button className={`btn ${currentPage === i ? "btn-active" : ""}`} key={i} onClick={handleChangePage}>{i}</button>)
             }
             return numbers
-        }
-    }
+        } }
     return (
         <>
             <div className="helpPage">
-                <div className="helpPage__organisation">
-                    <div className="organisation__description">
-                        <h4 className="description__title">{institutions[0].name}</h4>
-                        <p className="description__text">{institutions[0].goal}</p>
-                    </div>
-                    <p className="organisation__things">{institutions[0].items}</p>
-                </div>
-                <div className="helpPage__organisation">
-                    <div className="organisation__description">
-                        <h4 className="description__title">{institutions[1].name}</h4>
-                        <p className="description__text">{institutions[1].goal}</p>
-                    </div>
-                    <p className="organisation__things">{institutions[1].items}</p>
-                </div>
-                <div className="helpPage__organisation">
-                    <div className="organisation__description">
-                        <h4 className="description__title">{institutions[2].name}</h4>
-                        <p className="description__text">{institutions[2].goal}</p>
-                    </div>
-                    <p className="organisation__things">{institutions[2].items}</p>
-                </div>
+                {pageSet.map(verse => <OrganizationsVerse verse={verse} key={verse.name}/>)}
             </div>
-
             <div className="pageNumbers">
                 {pages()}
             </div>
