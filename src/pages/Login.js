28a8baf22527b274540c476d8c406cadd887aad1
duszaemilia <dusza.emilia@gmail.com>
2023-07-18 22:06:27 +0200

    Login layout and styling

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,34 @@
 import React from "react";
+import separator from "../assets/Decoration.svg"
+import LoginRegister from "../components/Home/HomeHeader/LoginRegister";
+import Navigation from "../components/Home/HomeHeader/Navigation";
 
-export default function Login() {
+export default function LoginView() {
     return (
-        <p>Login</p>
-    )
+    <section className="login">
+        <div className="login__navBox">
+            <LoginRegister/>
+            <Navigation/>
+        </div>
+        <form className="login__form">
+            <h2 className="form__title">Zaloguj się</h2>
+            <img src={separator} alt="separator" className="separator"/>
+            <div className="form__inputs">
+                <div className="inputs__email inputs">
+                    <label htmlFor="email" className="email-label">Email</label>
+                    <input type="text" id="email" name="email"/>
+                </div>
+                <div className="inputs__password inputs">
+                    <label htmlFor="password" className="password-label">Hasło</label>
+                    <input type="text" id="password" name="password"/>
+                </div>
+            </div>
+            <div className="form__buttons">
+                <button className="btn">Załóż konto</button>
+                <button className="btn btn-active">Zaloguj się</button>
+            </div>
+        </form>
+
+    </section>
+)
 }
