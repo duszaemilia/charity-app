bea45b80ca652d8882ab1e39da557910290fe006
duszaemilia <dusza.emilia@gmail.com>
2023-07-20 00:22:41 +0200

    Login, Logout, Signup layout and styling

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,34 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import separator from "../assets/Decoration.svg"
 import LoginRegister from "../components/Home/HomeHeader/LoginRegister";
 import Navigation from "../components/Home/HomeHeader/Navigation";
+import {Link} from "react-router-dom";
 
-export default function LoginView() {
+export default function Login() {
+    const [loader, setLoader] = useState("loading")
+
     return (
-    <section className="login">
-        <div className="login__navBox">
-            <LoginRegister/>
-            <Navigation/>
-        </div>
-        <form className="login__form">
-            <h2 className="form__title">Zaloguj się</h2>
-            <img src={separator} alt="separator" className="separator"/>
-            <div className="form__inputs">
-                <div className="inputs__email inputs">
-                    <label htmlFor="email" className="email-label">Email</label>
-                    <input type="text" id="email" name="email"/>
-                </div>
-                <div className="inputs__password inputs">
-                    <label htmlFor="password" className="password-label">Hasło</label>
-                    <input type="text" id="password" name="password"/>
-                </div>
+        <section className="login">
+            <div className="loginRegister__navBox">
+                <LoginRegister/>
+                <Navigation/>
             </div>
-            <div className="form__buttons">
-                <button className="btn">Załóż konto</button>
-                <button className="btn btn-active">Zaloguj się</button>
-            </div>
-        </form>
+            <form className="loginRegister__form">
+                <h2 className="form__title">Zaloguj się</h2>
+                <img src={separator} alt="separator" className="separator" onLoad={() => setLoader('loaded')}/>
+                {loader === 'loaded' ? <>
+                    <div className="form__inputs">
+                        <div className="inputs__email inputs">
+                            <label htmlFor="email" className="email-label">Email</label>
+                            <input type="text" id="email" name="email"/>
+                        </div>
+                        <div className="inputs__password inputs">
+                            <label htmlFor="password" className="password-label">Hasło</label>
+                            <input type="text" id="password" name="password"/>
+                        </div>
+                    </div>
+                    <div className="form__buttons">
+                        <Link to="/rejestracja">
+                            <button className="btn">Załóż konto</button>
+                        </Link>
+                        <button className="btn btn-active">Zaloguj się</button>
+                    </div>
+                </> : <></>}
+            </form>
 
-    </section>
-)
+        </section>
+    )
 }
--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
+import separator from "../assets/Decoration.svg";
+import LoginRegister from "../components/Home/HomeHeader/LoginRegister";
+import Navigation from "../components/Home/HomeHeader/Navigation";
+import {Link} from "react-router-dom";
 
 export default function Logout() {
+    const [loader, setLoader] = useState("loading")
+
     return (
-        <div>Logout</div>
+        <section className="logout">
+            <div className="loginRegister__navBox">
+                <LoginRegister/>
+                <Navigation/>
+            </div>
+            <form className="loginRegister__form">
+                <h2 className="form__title">Wylogowanie nastąpiło pomyślnie!</h2>
+                <img src={separator} alt="separator" className="separator" onLoad={() => setLoader('loaded')}/>
+                {loader === 'loaded' ? <>
+                    <div className="logout-button">
+                        <Link to="/"><button className="btn btn-active">Strona główna</button></Link>
+                    </div>
+                </> : <></>}
+            </form>
+        </section>
     )
 }
