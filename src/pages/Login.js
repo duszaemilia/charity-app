743f6943c40a6daf54176614d85223ef9365c1f8
duszaemilia <dusza.emilia@gmail.com>
2023-07-22 23:33:32 +0200

    loginform validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,42 +13,69 @@ export default function Login() {
 
     const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
+// Lokalny stan do przechowywania email i password
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [validation, setValidation] = useState([])
+    const handleLogin = (e) => {
+        e.preventDefault();
+        // Wykonanie walidacji
+        const sendOrNot = [];
 
-    const handleLogin = () => {
-        const userData = {name: "xyz", email: "xyz@xyz.com"};
-        dispatch(loginSuccess(userData));
-    };
+        if (email.length === 0 || email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) === null) {
+            sendOrNot.push("invalidEmail")
+        }
 
-    return (
-        <section className="login">
-            <div className="loginRegister__navBox">
-                <LoginRegister/>
-                <Navigation/>
+        if (password.length < 6) {
+            sendOrNot.push("invalidPassword")
+        }
+
+        setValidation([...sendOrNot])
+    }
+
+
+    // Przygotowanie obiektu z danymi użytkownika
+    const userData = {email, password};
+    // Wywołanie akcji loginSuccess i przekazanie danych użytkownika
+    dispatch(loginSuccess(userData));
+
+
+return (
+    <section className="login">
+        <div className="loginRegister__navBox">
+            <LoginRegister/>
+            <Navigation/>
+        </div>
+        <form className="loginRegister__form" onSubmit={handleLogin}>
+            <h2 className="form__title">Zaloguj się</h2>
+            <img src={separator} alt="separator" className="separator"/>
+            <div className="form__inputs">
+                <div className="inputs__email inputs">
+                    <label htmlFor="email" className="email-label">Email</label>
+                    <input type="text" id="email" name="email"
+                           className={`${validation.includes('invalidEmail') ? 'invalid' : 'valid'}`}
+                           onChange={(e) => setEmail(e.target.value)} value={email}/>
+                    <p className={`${validation.includes('invalidEmail') ? 'visible' : ''} error-message`}>Podany email
+                        jest nieprawidłowy!</p>
+                </div>
+                <div className="inputs__password inputs">
+                    <label htmlFor="password" className="password-label">Hasło</label>
+                    <input type="password" id="password" name="password"
+                           className={`${validation.includes('invalidPassword') ? 'invalid' : 'valid'}`}
+                           onChange={(e) => setPassword(e.target.value)} value={password}/>
+                    <p className={`${validation.includes('invalidPassword') ? 'visible' : ''} error-message`}>Podane
+                        hasło jest za krótkie!</p>
+                </div>
             </div>
-            <form className="loginRegister__form">
-                <h2 className="form__title">Zaloguj się</h2>
-                <img src={separator} alt="separator" className="separator"/>
-                <div className="form__inputs">
-                    <div className="inputs__email inputs">
-                        <label htmlFor="email" className="email-label">Email</label>
-                        <input type="text" id="email" name="email"/>
-                    </div>
-                    <div className="inputs__password inputs">
-                        <label htmlFor="password" className="password-label">Hasło</label>
-                        <input type="text" id="password" name="password"/>
-                    </div>
-                </div>
-                <div className="form__buttons">
-                    <Link to="/rejestracja">
-                        <button className="btn">Załóż konto</button>
-                    </Link>
-                    <button className="btn btn-active" onClick={handleLogin}>
-                        Zaloguj się
-                    </button>
-                </div>
-            </form>
-        </section>
-    );
+            <div className="form__buttons">
+                <Link to="/rejestracja">
+                    <button className="btn">Załóż konto</button>
+                </Link>
+                <button className="btn btn-active" type="submit">Zaloguj się</button>
+            </div>
+        </form>
+    </section>
+);
 }
 
 
